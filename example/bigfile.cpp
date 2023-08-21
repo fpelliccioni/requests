818cd7d7d00d994feb90f5d98149502d086b9add
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-08-21 23:48:03 +0200

    wip

--- example/bigfile.cpp
+++ example/bigfile.cpp
@@ -18,31 +18,46 @@
 using namespace boost;
 
 // curl "http://localhost:8080/bigfile?total_size=100000000&chunk_size=5000&delay_ms=10" --output output.bin
+// curl "http://192.168.1.117:8080/bigfile?total_size=100000000&chunk_size=5000&delay_ms=10" --output output.bin
+// curl "http://192.168.1.117:8080/timestamp"
 
 int main(int argc, char * argv[]) {
   asio::io_context ctx;
   auto tk = asio::bind_executor(ctx, asio::use_future);
 
+  requests::session hc{ctx};
+  hc.options().enforce_tls = false;
+  requests::default_session().options().enforce_tls = false;
+
   using FutureResponseType = decltype(requests::async_get(urls::url_view{"http://bla.com"}, {}, tk));
 
-  auto big_file_future = requests::async_get(urls::url_view{"http://localhost:8080/bigfile?total_size=10000000000&chunk_size=5000&delay_ms=100"}, {}, tk);
+  // std::cout << "before calling async_get bigfile" << std::endl;
+  // auto big_file_future = requests::async_get(hc, urls::url_view{"http://192.168.1.117:8080/bigfile?total_size=100000000&chunk_size=5000&delay_ms=10"}, {}, tk);
+
+  std::cout << "before calling async_get timestamp **" << std::endl;
+  auto r = requests::get(hc, urls::url_view("http://192.168.1.117:8080/timestamp"));
+  std::cout << as_json(r) << std::endl;
+
 
   std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-  std::vector<FutureResponseType> echo_futures;
-  for (int i = 0; i < 10; ++i) {
-      echo_futures.push_back(requests::async_get(urls::url_view{"http://localhost:8080/echo"}, {}, tk));
-  }
+  // std::vector<FutureResponseType> echo_futures;
+  // for (int i = 0; i < 10; ++i) {
+  //     std::cout << "before calling async_get timestamp" << std::endl;
+  //     echo_futures.push_back(requests::async_get(hc, urls::url_view{"http://192.168.1.117:8080/timestamp"}, {}, tk));
+  // }
 
   ctx.run();
 
-  for (auto &fut : echo_futures) {
-      auto response = fut.get();
-      std::cout << "Received echo response: " << response.headers << response.string_view() << std::endl;
-  }
+  // for (auto &fut : echo_futures) {
+  //   std::cout << "before .get timestamp future" << std::endl;
+  //   auto response = fut.get();
+  //   std::cout << "Received echo response: " << response.headers << response.string_view() << std::endl;
+  // }
 
-  auto big_response = big_file_future.get();
-  std::cout << "Received big file response. Size: " << big_response.string_view().size() << std::endl;
+  // std::cout << "before .get big_file future" << std::endl;
+  // auto big_response = big_file_future.get();
+  // std::cout << "Received big file response. Size: " << big_response.string_view().size() << std::endl;
 
   return 0;
 }
